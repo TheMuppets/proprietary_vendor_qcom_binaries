e1785530e1e7a5f36faaa601ed1ad488c60c5415
Ashwin Ramesh <ashwinr64@gmail.com>
2017-02-05 08:29:51 +0530

    qcom: Add common Widevine blobs
    * Taken from bullhead N4F26I
    * Most of the boards use these updated blobs , so just put it in common.

--- Android.mk
+++ Android.mk
@@ -4,4 +4,5 @@ include $(CLEAR_VARS)
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include $(call all-makefiles-under,$(LOCAL_PATH)/$(TARGET_BOARD_PLATFORM))
+include $(call all-makefiles-under,$(LOCAL_PATH)/widevine)
 endif
